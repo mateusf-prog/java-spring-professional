6f16fbce92646d207266831d91ee8459c11309db
mateusfprog <mateus102006@hotmail.com>
2024-05-05 09:56:54 -0300

    create classes Order, User and OrderStatus enum and relating

---
+++ src/main/java/com/mateus/dscommerce/domains/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.mateus.dscommerce.domains;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
---
+++ src/main/java/com/mateus/dscommerce/domains/User.java
@@ -0,0 +1,79 @@
+package com.mateus.dscommerce.domains;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthdate;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, LocalDate birthdate, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthdate = birthdate;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
