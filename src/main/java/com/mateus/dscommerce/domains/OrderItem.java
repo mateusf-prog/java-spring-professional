17594ca0314fd34ff885cf1adb4810eac39392cd
mateusfprog <mateus102006@hotmail.com>
2024-05-09 19:19:21 -0300

    create Equals and Hashcode to entities

--- src/main/java/com/mateus/dscommerce/domains/OrderItem.java
+++ src/main/java/com/mateus/dscommerce/domains/OrderItem.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem {
@@ -64,5 +66,19 @@ public class OrderItem {
 
     public void setOrder(Order order) {
         id.setOrder(order);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
