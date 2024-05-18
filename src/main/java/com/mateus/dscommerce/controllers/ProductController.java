2a11566fdd34530841f6f8067b3a595ae94490e3
mateusfprog <mateus102006@hotmail.com>
2024-05-18 13:14:45 -0300

    created DELETE enpoint in the ProductController

--- src/main/java/com/mateus/dscommerce/controllers/ProductController.java
+++ src/main/java/com/mateus/dscommerce/controllers/ProductController.java
@@ -52,4 +52,10 @@ public class ProductController {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/mateus/dscommerce/services/ProductService.java
+++ src/main/java/com/mateus/dscommerce/services/ProductService.java
@@ -54,4 +54,9 @@ public class ProductService {
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
     }
+
+    @Transactional()
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
