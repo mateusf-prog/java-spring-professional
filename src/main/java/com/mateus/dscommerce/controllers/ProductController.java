25831ee6e489c9ea71676d45b409f2cb8d29d093
mateusfprog <mateus102006@hotmail.com>
2024-05-18 12:04:04 -0300

    changed the endpoints of ProductController to return ResponseEntity

--- src/main/java/com/mateus/dscommerce/controllers/ProductController.java
+++ src/main/java/com/mateus/dscommerce/controllers/ProductController.java
@@ -4,8 +4,11 @@ import com.mateus.dscommerce.dto.ProductDTO;
 import com.mateus.dscommerce.services.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,17 +22,28 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ProductDTO findById(@PathVariable Long id) {
-        return service.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping()
-    public Page<ProductDTO> findAll(Pageable pageable) {
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 
+    /**
+     * Inserindo um objeto no banco de dados, retornando o objeto, sua URI de criação e
+     * o código 201 CREATED
+     * @param dto
+     * @return
+     */
     @PostMapping()
-    public ProductDTO insert(@RequestBody ProductDTO dto) {
-        return service.insert(dto);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto =  service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
